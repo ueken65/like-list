39e0ec8bff7cb5e7ee2498e51f7b0a253eea42cf
ueken65 <ueken6522@gmail.com>
2019-03-30 16:22:29 +0900

    CDNからソースを読み込むようにした

---
+++ js/loadCreateLikeUserList.js
@@ -0,0 +1,8 @@
+var scriptElement = document.createElement("script");
+var styleElement = document.createElement("link");
+
+scriptElement.src = "https://cdn.jsdelivr.net/gh/ueken65/like-list@master/js/createLikeUserList.js";
+styleElement.href = "https://cdn.jsdelivr.net/gh/ueken65/like-list@master/css/style.js";
+
+document.body.appendChild(scriptElement);
+document.body.appendChild(styleElement);
