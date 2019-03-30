d5be5ad79305f3199ca664e8d9ef239fe423ab9b
ueken65 <ueken6522@gmail.com>
2019-03-31 00:50:22 +0900

    デザイン・動作調整

--- js/createLikeUserList.js
+++ js/createLikeUserList.js
@@ -1,39 +1,75 @@
 window.onload = function(){
-    const p = document.createElement('div');
-    const text = document.createTextNode('button');
-
-    // プロパティ、値の設定 
-
-    // ノードの挿入
-    p.id = 'csv_button';
-    p.appendChild(text);
-    document.body.appendChild(p);
-
+    
+    //init
+    addListNode();
 
     var likeList = [];
     var exportFlg = true;
 
     document.addEventListener('DOMNodeInserted', function() {
-        var targets = document.querySelectorAll('div[role="dialog"] div div div div div a[title]');
+        var targets = document.querySelectorAll('div[role="dialog"] a[title]');
+        if(targets.length === 0){
+            return;
+        }
+
         var username = targets[targets.length - 1].getAttribute('title');
         if(likeList.includes(username)){
             return;
         }
         likeList.push(username);
         exportFlg = true;
-        // console.log(likeList.length);
     }, false);
 
-    p.onclick = function(){
+    // ボタンをクリックしたらリスト作成
+    var createListButton = document.getElementById('createListButton');
+    createListButton.onclick = function(){
         if(!exportFlg){
             return;
         }
         var htmlList = '';
             for (var i=0; i<likeList.length;i++){
-            htmlList += '<p>'+ likeList[i] + '</p>';　// = ではなく += を使う
+            htmlList += '<li>'+ likeList[i] + '</li>';
         }
-        document.getElementById('csv_button').innerHTML = htmlList;
+        document.getElementById('userListWrap').innerHTML = htmlList;
         exportFlg = false;
         likeList = [];
     }
+
+    // リストをクリックしたら選択してコピー
+    var userListWrap = document.getElementById('userListWrap');
+    userListWrap.onclick = function(){
+        selectDomElm(this);
+        document.execCommand('copy');
+    }
 };
+
+// リストセクション（リスト生成ボタン・リスト領域）ノードの作成
+function addListNode(){
+    const listSection = document.createElement('div');
+    const createListButton = document.createElement('a');
+    const buttonText = document.createTextNode('リスト作成');
+    const userListWrap = document.createElement('ul');
+
+    listSection.id = 'listSection';
+    
+    //ボタン追加
+    createListButton.appendChild(buttonText);
+    createListButton.id = 'createListButton';
+    listSection.appendChild(createListButton);
+    
+    //リストのwrap追加
+    userListWrap.id = 'userListWrap';
+    listSection.appendChild(userListWrap);
+    
+    document.body.appendChild(listSection);        
+}
+
+// クリックするとリストを選択・コピー
+function selectDomElm(obj){
+    var range = document.createRange();
+    range.selectNodeContents(obj);
+   
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+   }
