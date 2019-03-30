ef74d2e5e12f0a6bdeab5dc9cf8ee66ed06653d9
ueken65 <ueken6522@gmail.com>
2019-03-30 20:56:40 +0900

    cssの読み込み修正

--- js/loadCreateLikeUserList.js
+++ js/loadCreateLikeUserList.js
@@ -1,8 +1,9 @@
-var scriptElement = document.createElement("script");
 var styleElement = document.createElement("link");
+var scriptElement = document.createElement("script");
 
+styleElement.href = "https://cdn.jsdelivr.net/gh/ueken65/like-list@master/css/style.css";
+styleElement.rel = "stylesheet";
 scriptElement.src = "https://cdn.jsdelivr.net/gh/ueken65/like-list@master/js/createLikeUserList.js";
-styleElement.href = "https://cdn.jsdelivr.net/gh/ueken65/like-list@master/css/style.js";
 
-document.body.appendChild(scriptElement);
 document.body.appendChild(styleElement);
+document.body.appendChild(scriptElement);
