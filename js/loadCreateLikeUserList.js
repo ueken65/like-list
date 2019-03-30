50e1d38969b3a96b195670a9e987778d5f3f8dfe
ueken65 <ueken6522@gmail.com>
2019-03-31 01:05:57 +0900

    1.0

--- js/loadCreateLikeUserList.js
+++ js/loadCreateLikeUserList.js
@@ -1,9 +1,9 @@
 var styleElement = document.createElement("link");
 var scriptElement = document.createElement("script");
 
-styleElement.href = "https://cdn.jsdelivr.net/gh/ueken65/like-list@master/css/style.css";
+styleElement.href = "https://cdn.jsdelivr.net/gh/ueken65/like-list@1.0/css/style.css";
 styleElement.rel = "stylesheet";
-scriptElement.src = "https://cdn.jsdelivr.net/gh/ueken65/like-list@master/js/createLikeUserList.js";
+scriptElement.src = "https://cdn.jsdelivr.net/gh/ueken65/like-list@1.0/js/createLikeUserList.js";
 
 document.body.appendChild(styleElement);
 document.body.appendChild(scriptElement);
